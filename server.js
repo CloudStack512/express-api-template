9cfe0bbbf39b9276f29d0542e40773864160fadd
Matthew <fsjs25@icloud.com>
2025-10-05 10:47:10 -0400

    update routes

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/protected", require("./routes/protectedRoutes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
